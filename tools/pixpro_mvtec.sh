bbef74fb99f2420c52ab4223620f9b20e99cefbb
Haitian He <haitianh@2073L-207327-M.local>
2022-02-19 10:28:11 +1100

    update

--- tools/pixpro_mvtec.sh
+++ tools/pixpro_mvtec.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-number_of_processes=4
+number_of_processes=8
 data_dir="./data/mvtec/"
 output_dir="./output/pixpro_mvtec"
 
@@ -19,29 +19,29 @@ python -m torch.distributed.launch --master_port 12350 --nproc_per_node=${number
     --aug BYOL \
     --dataset MVTec \
     --batch-size 64 \
-    --image-size 32 \
+    --image-size 224 \
     --resized_image_size 256 \
-    --crop_patch_size 128 \
-    --crop_patch_stride 64 \
+    --crop_patch_size 256 \
+    --crop_patch_stride 256 \
     \
     --model PixPro \
     --arch resnet18 \
     --head-type early_return \
     \
     --optimizer lars \
-    --base-lr 0.01 \
+    --base-lr 0.1 \
     --weight-decay 0.0003 \
     --warmup-epoch 5 \
-    --epochs 100 \
+    --epochs 400 \
     --amp-opt-level O1 \
     \
-    --save-freq 50 \
+    --save-freq 100 \
     --auto-resume \
     \
     --pixpro-p 2 \
     --pixpro-momentum 0.99 \
     --pixpro-pos-ratio 0.7 \
     --pixpro-transform-layer 1 \
-    --pixpro-ins-loss-weight 1 \
+    --pixpro-ins-loss-weight 0 \
     \
     --mvtec_category all 
